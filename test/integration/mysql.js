864a90e89fd36ea5c0be3624c99d38379593741a
Diogo Resende <dresende@thinkdigital.pt>
2013-09-19 18:23:56 +0100

    Updates code and changes types to match ORM

--- test/integration/mysql.js
+++ test/integration/mysql.js
@@ -7,18 +7,24 @@ describe("MySQL.getType", function () {
 	it("should detect text", function (done) {
 		Dialect.getType({ type: "text" }).should.equal("VARCHAR(255)");
 		Dialect.getType({ type: "text", size: 150 }).should.equal("VARCHAR(150)");
+		Dialect.getType({ type: "text", size: 1000 }).should.equal("VARCHAR(1000)");
 
 		return done();
 	});
 
 	it("should detect numbers", function (done) {
-		Dialect.getType({ type: "number" }).should.equal("INT(5)");
+		Dialect.getType({ type: "number" }).should.equal("INTEGER");
+		Dialect.getType({ type: "number", size: 4 }).should.equal("INTEGER");
+		Dialect.getType({ type: "number", size: 2 }).should.equal("SMALLINT");
+		Dialect.getType({ type: "number", size: 8 }).should.equal("BIGINT");
 
 		return done();
 	});
 
 	it("should detect rational numbers", function (done) {
 		Dialect.getType({ type: "number", rational: true }).should.equal("FLOAT");
+		Dialect.getType({ type: "number", rational: true, size: 4 }).should.equal("FLOAT");
+		Dialect.getType({ type: "number", rational: true, size: 8 }).should.equal("DOUBLE");
 
 		return done();
 	});
@@ -29,8 +35,38 @@ describe("MySQL.getType", function () {
 		return done();
 	});
 
+	it("should detect dates", function (done) {
+		Dialect.getType({ type: "date" }).should.equal("DATE");
+
+		return done();
+	});
+
+	it("should detect dates with times", function (done) {
+		Dialect.getType({ type: "date", time: true }).should.equal("DATETIME");
+
+		return done();
+	});
+
+	it("should detect binary", function (done) {
+		Dialect.getType({ type: "binary" }).should.equal("BLOB");
+
+		return done();
+	});
+
+	it("should detect big binary", function (done) {
+		Dialect.getType({ type: "binary", big: true }).should.equal("LONGBLOB");
+
+		return done();
+	});
+
 	it("should detect required items", function (done) {
 		Dialect.getType({ type: "boolean", required: true }).should.match(/NOT NULL/);
+
+		return done();
+	});
+
+	it("should detect default values", function (done) {
+		Dialect.getType({ type: "number", defaultValue: 3 }).should.match(/DEFAULT 3/);
 
 		return done();
 	});
