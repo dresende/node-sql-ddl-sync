87531f90e597506de422b55496d05406164d42e1
Diogo Resende <dresende@thinkdigital.pt>
2013-09-19 23:43:39 +0100

    Changes dialect.getType to pass table and column name, returns object
    This is to prepare for postgresql needs

--- test/integration/mysql.js
+++ test/integration/mysql.js
@@ -5,77 +5,77 @@ var should  = require("should");
 
 describe("MySQL.getType", function () {
 	it("should detect text", function (done) {
-		Dialect.getType({ type: "text" }).should.equal("VARCHAR(255)");
-		Dialect.getType({ type: "text", size: 150 }).should.equal("VARCHAR(150)");
-		Dialect.getType({ type: "text", size: 1000 }).should.equal("VARCHAR(1000)");
+		Dialect.getType(null, null, { type: "text" }).value.should.equal("VARCHAR(255)");
+		Dialect.getType(null, null, { type: "text", size: 150 }).value.should.equal("VARCHAR(150)");
+		Dialect.getType(null, null, { type: "text", size: 1000 }).value.should.equal("VARCHAR(1000)");
 
 		return done();
 	});
 
 	it("should detect numbers", function (done) {
-		Dialect.getType({ type: "number" }).should.equal("INTEGER");
-		Dialect.getType({ type: "number", size: 4 }).should.equal("INTEGER");
-		Dialect.getType({ type: "number", size: 2 }).should.equal("SMALLINT");
-		Dialect.getType({ type: "number", size: 8 }).should.equal("BIGINT");
+		Dialect.getType(null, null, { type: "number" }).value.should.equal("INTEGER");
+		Dialect.getType(null, null, { type: "number", size: 4 }).value.should.equal("INTEGER");
+		Dialect.getType(null, null, { type: "number", size: 2 }).value.should.equal("SMALLINT");
+		Dialect.getType(null, null, { type: "number", size: 8 }).value.should.equal("BIGINT");
 
 		return done();
 	});
 
 	it("should detect rational numbers", function (done) {
-		Dialect.getType({ type: "number", rational: true }).should.equal("FLOAT");
-		Dialect.getType({ type: "number", rational: true, size: 4 }).should.equal("FLOAT");
-		Dialect.getType({ type: "number", rational: true, size: 8 }).should.equal("DOUBLE");
+		Dialect.getType(null, null, { type: "number", rational: true }).value.should.equal("FLOAT");
+		Dialect.getType(null, null, { type: "number", rational: true, size: 4 }).value.should.equal("FLOAT");
+		Dialect.getType(null, null, { type: "number", rational: true, size: 8 }).value.should.equal("DOUBLE");
 
 		return done();
 	});
 
 	it("should detect booleans", function (done) {
-		Dialect.getType({ type: "boolean" }).should.equal("TINYINT(1)");
+		Dialect.getType(null, null, { type: "boolean" }).value.should.equal("TINYINT(1)");
 
 		return done();
 	});
 
 	it("should detect dates", function (done) {
-		Dialect.getType({ type: "date" }).should.equal("DATE");
+		Dialect.getType(null, null, { type: "date" }).value.should.equal("DATE");
 
 		return done();
 	});
 
 	it("should detect dates with times", function (done) {
-		Dialect.getType({ type: "date", time: true }).should.equal("DATETIME");
+		Dialect.getType(null, null, { type: "date", time: true }).value.should.equal("DATETIME");
 
 		return done();
 	});
 
 	it("should detect binary", function (done) {
-		Dialect.getType({ type: "binary" }).should.equal("BLOB");
+		Dialect.getType(null, null, { type: "binary" }).value.should.equal("BLOB");
 
 		return done();
 	});
 
 	it("should detect big binary", function (done) {
-		Dialect.getType({ type: "binary", big: true }).should.equal("LONGBLOB");
+		Dialect.getType(null, null, { type: "binary", big: true }).value.should.equal("LONGBLOB");
 
 		return done();
 	});
 
 	it("should detect required items", function (done) {
-		Dialect.getType({ type: "boolean", required: true }).should.match(/NOT NULL/);
+		Dialect.getType(null, null, { type: "boolean", required: true }).value.should.match(/NOT NULL/);
 
 		return done();
 	});
 
 	it("should detect default values", function (done) {
-		Dialect.getType({ type: "number", defaultValue: 3 }).should.match(/DEFAULT 3/);
+		Dialect.getType(null, null, { type: "number", defaultValue: 3 }).value.should.match(/DEFAULT 3/);
 
 		return done();
 	});
 
 	it("should detect serial", function (done) {
-		var column = Dialect.getType({
+		var column = Dialect.getType(null, null, {
 			type   : "number",
 			serial : true
-		});
+		}).value;
 
 		column.should.match(/INT/);
 		column.should.match(/NOT NULL/);
