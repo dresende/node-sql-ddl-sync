cb532a7977d2265474cfa481adfabdf0157c105a
Arek W <arek01@gmail.com>
2014-05-07 20:53:19 +1000

    Test fixes

--- test/integration/postgresql.js
+++ test/integration/postgresql.js
@@ -5,63 +5,63 @@ var driver  = common.fakeDriver;
 
 describe("PostgreSQL.getType", function () {
 	it("should detect text", function (done) {
-		Dialect.getType(null, null, { type: "text" }, driver).value.should.equal("TEXT");
-		Dialect.getType(null, null, { type: "text", size: 150 }, driver).value.should.equal("TEXT");
-		Dialect.getType(null, null, { type: "text", size: 1000 }, driver).value.should.equal("TEXT");
+		Dialect.getType(null, { mapsTo: 'abc',  type: "text" }, driver).value.should.equal("TEXT");
+		Dialect.getType(null, { mapsTo: 'abc',  type: "text", size: 150 }, driver).value.should.equal("TEXT");
+		Dialect.getType(null, { mapsTo: 'abc',  type: "text", size: 1000 }, driver).value.should.equal("TEXT");
 
 		return done();
 	});
 
 	it("should detect numbers", function (done) {
-		Dialect.getType(null, null, { type: "integer" }, driver).value.should.equal("INTEGER");
-		Dialect.getType(null, null, { type: "integer", size: 4 }, driver).value.should.equal("INTEGER");
-		Dialect.getType(null, null, { type: "integer", size: 2 }, driver).value.should.equal("SMALLINT");
-		Dialect.getType(null, null, { type: "integer", size: 8 }, driver).value.should.equal("BIGINT");
-		Dialect.getType(null, null, { type: "number", rational: false }, driver).value.should.equal("INTEGER");
+		Dialect.getType(null, { mapsTo: 'abc', type: "integer" }, driver).value.should.equal("INTEGER");
+		Dialect.getType(null, { mapsTo: 'abc', type: "integer", size: 4 }, driver).value.should.equal("INTEGER");
+		Dialect.getType(null, { mapsTo: 'abc', type: "integer", size: 2 }, driver).value.should.equal("SMALLINT");
+		Dialect.getType(null, { mapsTo: 'abc', type: "integer", size: 8 }, driver).value.should.equal("BIGINT");
+		Dialect.getType(null, { mapsTo: 'abc', type: "number", rational: false }, driver).value.should.equal("INTEGER");
 
 		return done();
 	});
 
 	it("should detect rational numbers", function (done) {
-		Dialect.getType(null, null, { type: "number"}, driver).value.should.equal("REAL");
-		Dialect.getType(null, null, { type: "number", size: 4 }, driver).value.should.equal("REAL");
-		Dialect.getType(null, null, { type: "number", size: 8 }, driver).value.should.equal("DOUBLE PRECISION");
+		Dialect.getType(null, { mapsTo: 'abc', type: "number"}, driver).value.should.equal("REAL");
+		Dialect.getType(null, { mapsTo: 'abc', type: "number", size: 4 }, driver).value.should.equal("REAL");
+		Dialect.getType(null, { mapsTo: 'abc', type: "number", size: 8 }, driver).value.should.equal("DOUBLE PRECISION");
 
 		return done();
 	});
 
 	it("should detect booleans", function (done) {
-		Dialect.getType(null, null, { type: "boolean" }, driver).value.should.equal("BOOLEAN");
+		Dialect.getType(null, { mapsTo: 'abc', type: "boolean" }, driver).value.should.equal("BOOLEAN");
 
 		return done();
 	});
 
 	it("should detect dates", function (done) {
-		Dialect.getType(null, null, { type: "date" }, driver).value.should.equal("DATE");
+		Dialect.getType(null, { mapsTo: 'abc', type: "date" }, driver).value.should.equal("DATE");
 
 		return done();
 	});
 
 	it("should detect dates with times", function (done) {
-		Dialect.getType(null, null, { type: "date", time: true }, driver).value.should.equal("TIMESTAMP WITHOUT TIME ZONE");
+		Dialect.getType(null, { mapsTo: 'abc', type: "date", time: true }, driver).value.should.equal("TIMESTAMP WITHOUT TIME ZONE");
 
 		return done();
 	});
 
 	it("should detect binary", function (done) {
-		Dialect.getType(null, null, { type: "binary" }, driver).value.should.equal("BYTEA");
+		Dialect.getType(null, { mapsTo: 'abc', type: "binary" }, driver).value.should.equal("BYTEA");
 
 		return done();
 	});
 
 	it("should detect required items", function (done) {
-		Dialect.getType(null, null, { type: "boolean", required: true }, driver).value.should.match(/NOT NULL/);
+		Dialect.getType(null, { mapsTo: 'abc', type: "boolean", required: true }, driver).value.should.match(/NOT NULL/);
 
 		return done();
 	});
 
 	it("should detect default values", function (done) {
-		Dialect.getType(null, null, { type: "number", defaultValue: 3 }, driver).value.should.match(/DEFAULT \^\^3\^\^/);
+		Dialect.getType(null, { mapsTo: 'abc', type: "number", defaultValue: 3 }, driver).value.should.match(/DEFAULT \^\^3\^\^/);
 
 		return done();
 	});
